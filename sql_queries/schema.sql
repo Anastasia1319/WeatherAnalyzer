3e299455bc14f96e246c11634e2be8df28ba55c6
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-03-20 16:19:25 +0300

    Created schema.sql in sql_queries package

---
+++ sql_queries/schema.sql
@@ -0,0 +1,18 @@
+/*
+ DROP IF EXISTS TABLE weather_info;
+*/
+
+
+CREATE TABLE IF NOT EXISTS weather_info
+(
+    id BIGSERIAL PRIMARY KEY,
+    country VARCHAR(100),
+    locality VARCHAR(200),
+    "date" DATE,
+    local_time TIME,
+    temperature_c DECIMAL(3, 1),
+    wind_speed_m_in_s DECIMAL(3, 1),
+    atmospheric_pressure_hPa DECIMAL(7, 3),
+    air_humidity INTEGER,
+    weather_conditions VARCHAR(100)
+);
