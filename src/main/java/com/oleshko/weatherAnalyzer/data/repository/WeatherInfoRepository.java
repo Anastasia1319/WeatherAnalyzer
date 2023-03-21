469bfb5e5d27fee228574fcf5d843e1f5d340a77
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-03-21 21:36:40 +0300

    Added method to search for current weather

--- src/main/java/com/oleshko/weatherAnalyzer/data/repository/WeatherInfoRepository.java
+++ src/main/java/com/oleshko/weatherAnalyzer/data/repository/WeatherInfoRepository.java
@@ -2,6 +2,13 @@ package com.oleshko.weatherAnalyzer.data.repository;
 
 import com.oleshko.weatherAnalyzer.data.entity.WeatherInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface WeatherInfoRepository extends JpaRepository<WeatherInfo, Long> {
+
+    Optional<WeatherInfo> findFirstByOrderByDateTimeDesc();
+
 }
