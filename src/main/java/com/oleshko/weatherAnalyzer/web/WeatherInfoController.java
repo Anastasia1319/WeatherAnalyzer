b1d5d47f756a6e1643d753c99665ce7387ae4185
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-03-23 16:10:38 +0300

    Added statuses in WeatherInfoController

--- src/main/java/com/oleshko/weatherAnalyzer/web/WeatherInfoController.java
+++ src/main/java/com/oleshko/weatherAnalyzer/web/WeatherInfoController.java
@@ -4,7 +4,7 @@ import com.oleshko.weatherAnalyzer.service.WeatherInfoService;
 import com.oleshko.weatherAnalyzer.service.dto.AverageTemperatureDto;
 import com.oleshko.weatherAnalyzer.service.dto.WeatherInfoDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -17,11 +17,13 @@ public class WeatherInfoController {
     private final WeatherInfoService weatherInfoService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public WeatherInfoDto get() {
         return weatherInfoService.getActualWeather();
     }
 
     @PostMapping("/average")
+    @ResponseStatus(HttpStatus.OK)
     public AverageTemperatureDto getAverage(@RequestBody LocalDate dateFrom, @RequestBody LocalDate dateTo) {
         return weatherInfoService.getAverageTemperature(dateFrom, dateTo);
     }
