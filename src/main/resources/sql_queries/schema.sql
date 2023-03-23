9aa0e76b3f6fc2b7a081a1b90fff4ce7ebc52a9b
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-03-23 15:20:43 +0300

    Fixed SQL schema

--- src/main/resources/sql_queries/schema.sql
+++ src/main/resources/sql_queries/schema.sql
@@ -8,27 +8,10 @@ CREATE TABLE IF NOT EXISTS weather_info
     id BIGSERIAL PRIMARY KEY,
     country VARCHAR(100),
     locality VARCHAR(200),
-    "date" DATE,
-    local_time TIME,
-    temperature_c DECIMAL(3, 1),
-    wind_speed_m_in_s DECIMAL(3, 1),
-    atmospheric_pressure_hPa DECIMAL(7, 3),
-    air_humidity INTEGER,
-    weather_condition VARCHAR(100)
-);
-
-CREATE TABLE IF NOT EXISTS weather_info
-(
-    id BIGSERIAL PRIMARY KEY,
-    country VARCHAR(100),
-    locality VARCHAR(200),
     date_time TIMESTAMP,
     temperature_c DECIMAL(3, 1),
     wind_speed_m_in_s DECIMAL(3, 1),
     atmospheric_pressure_hPa DECIMAL(7, 3),
     air_humidity INTEGER,
     weather_condition VARCHAR(100)
-    );
-
-INSERT INTO weather_info (country, locality, date_time, temperature_c, wind_speed_m_in_s, atmospheric_pressure_hPa, air_humidity, weather_condition)
-VALUES ('Belarus', 'Minsk', '1997-12-17 07:37:16-08', 15.0, 1.2, 1195.1, 60, 'aaaa');
+);
